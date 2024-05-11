2c653d31ccf7bbaeccda81921310e2eb9c61a7c2
Dora <kondaladorababu@gmail.com>
2024-05-11 11:47:10 +0530

    Initial Project Set Up

---
+++ src/main/java/com/onePlace/onePlaceBackend/OnePlaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.onePlace.onePlaceBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePlaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnePlaceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onePlace/onePlaceBackend/OnePlaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onePlace.onePlaceBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePlaceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
